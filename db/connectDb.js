9136dc3729487b86779e0a807e354f943cc57519
Shivanshu Shekhar Jha <shivanshu1221@gmail.com>
2026-02-10 11:55:07 +0530

    updated connectDb

--- db/connectDb.js
+++ db/connectDb.js
@@ -5,7 +5,8 @@ const connectDb = async () => {
         const conn = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        }); 
+        
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error.message);
