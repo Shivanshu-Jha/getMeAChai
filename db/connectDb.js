d54343467314afc1130d77972accc961dee6ccc7
Shivanshu Shekhar Jha <shivanshu1221@gmail.com>
2026-02-10 12:49:05 +0530

    updated mongoDb connection

--- db/connectDb.js
+++ db/connectDb.js
@@ -1,44 +1,16 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const connectDb = async () => {
-//     try {
-//         const conn = await mongoose.connect(process.env.MONGODB_URI, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//         }); 
-
-//         console.log(`MongoDB connected: ${conn.connection.host}`);
-//     } catch (error) {
-//         console.error(error.message);
-//         process.exit(1); // Exit process with failure
-//     }
-// }
-// export default connectDb;
-// lib/mongodb.js
-
-import { MongoClient } from 'mongodb'
-
-const uri = process.env.MONGODB_URI
-
-const options = {}
-
-let client
-let clientPromise
-
-
-if (!process.env.MONGODB_URI) {
-    throw new Error('Add Mongo URI to .env.local')
-}
-
-if (process.env.NODE_ENV === 'development') {
-    if (!global._mongoClientPromise) {
-        client = new MongoClient(uri, options)
-        global._mongoClientPromise = client.connect()
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }); 
+        
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error.message);
+        process.exit(1); // Exit process with failure
     }
-    clientPromise = global._mongoClientPromise
-} else {
-    client = new MongoClient(uri, options)
-    clientPromise = client.connect()
 }
-
-export default clientPromise
+export default connectDb;
