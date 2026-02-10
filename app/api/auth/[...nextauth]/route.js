2fe34208ee7513e4e5120dcb9b89c89341347d0d
Shivanshu Shekhar Jha <shivanshu1221@gmail.com>
2026-02-10 12:24:48 +0530

    Initial Commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,89 +1,46 @@
-// import NextAuth from 'next-auth'
-// import GitHubProvider from "next-auth/providers/github";
-// import mongoose from 'mongoose';
-// import User from '@/models/User';
-// import Payment from '@/models/Payment';
-// import connectDb from '@/db/connectDb';
-
-// export const authoptions = NextAuth({
-//     providers: [
-//         // OAuth authentication providers...
-//         GitHubProvider({
-//             clientId: process.env.GITHUB_ID,
-//             clientSecret: process.env.GITHUB_SECRET,
-
-//         }),
-//     ],
-//     secret: process.env.NEXTAUTH_SECRET,
-
-//     callbacks: {
-//         async signIn({ user, account, profile, email, credentials }) {
-//             if (account.provider == 'github') {
-//                 await connectDb();
-//                 //check if user already exists
-//                 const currentUser = await User.findOne({ email: user.email })
-//                 if (!currentUser) {
-//                     //if not, create a new user
-//                     const newUser = await User.create({
-//                         email: user.email,
-//                         username: user.email.split('@')[0],
-//                     });
-//                 }
-
-//                 return true;
-//             }
-//         },
-//         async session({ session, user, token }) {
-//             const dbUser = await User.findOne({ email: session.user.email });
-//             console.log(dbUser);
-//             session.user.name = dbUser.username;
-//             return session
-//         },
-//     }
-// })
-
-// export { authoptions as GET, authoptions as POST }
-
-import NextAuth from "next-auth";
+import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
-import User from "@/models/User";
-import connectDb from "@/db/connectDb";
+import mongoose from 'mongoose';
+import User from '@/models/User';
+import Payment from '@/models/Payment';
+import connectDb from '@/db/connectDb';
 
-export const authOptions = {
+export const authoptions = {
     providers: [
+        // OAuth authentication providers...
         GitHubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET,
 
     callbacks: {
-        async signIn({ user, account }) {
-            if (account.provider === "github") {
+        async signIn({ user, account, profile, email, credentials }) {
+            if (account.provider == 'github') {
                 await connectDb();
-                const currentUser = await User.findOne({ email: user.email });
+                //check if user already exists
+                const currentUser = await User.findOne({ email: user.email })
                 if (!currentUser) {
-                    await User.create({
+                    //if not, create a new user
+                    const newUser = await User.create({
                         email: user.email,
-                        username: user.email.split("@")[0],
+                        username: user.email.split('@')[0],
                     });
                 }
+
                 return true;
             }
-            return false;
         },
-        async session({ session }) {
+        async session({ session, user, token }) {
             const dbUser = await User.findOne({ email: session.user.email });
-            if (dbUser) {
-                session.user.name = dbUser.username;
-            }
-            return session;
+            console.log(dbUser);
+            session.user.name = dbUser.username;
+            return session
         },
-    },
-};
+    }
+}
 
-
-const handler = NextAuth(authOptions);
-
+const handler = NextAuth(authoptions);
 export { handler as GET, handler as POST };
--- db/connectDb.js
+++ db/connectDb.js
@@ -1,16 +1,44 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectDb = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }); 
-        
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1); // Exit process with failure
-    }
+// const connectDb = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGODB_URI, {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//         }); 
+
+//         console.log(`MongoDB connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.error(error.message);
+//         process.exit(1); // Exit process with failure
+//     }
+// }
+// export default connectDb;
+// lib/mongodb.js
+
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI
+
+const options = {}
+
+let client
+let clientPromise
+
+
+if (!process.env.MONGODB_URI) {
+    throw new Error('Add Mongo URI to .env.local')
 }
-export default connectDb;
+
+if (process.env.NODE_ENV === 'development') {
+    if (!global._mongoClientPromise) {
+        client = new MongoClient(uri, options)
+        global._mongoClientPromise = client.connect()
+    }
+    clientPromise = global._mongoClientPromise
+} else {
+    client = new MongoClient(uri, options)
+    clientPromise = client.connect()
+}
+
+export default clientPromise
